793bc1ceae81fe7ca856825da7b83102314d4777
Andressa Daniele Prado <dapraviverdisso@gmail.com>
2025-08-30 08:11:53 -0300

    feat: finaliza configuracoes de deploy

--- app/api/leads/route.ts
+++ app/api/leads/route.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'; // Importa o cliente padrão
+import { createClient } from '@supabase/supabase-js'; 
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
